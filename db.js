f1bee3b9e5210616e9a12f68d305921db1ad51dc
Meghana Vegiraju <meghanavegiraju6@gmail.com>
2026-04-25 19:53:11 +0530

    updated db config for deployment

--- db.js
+++ db.js
@@ -1,11 +1,28 @@
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "india_geo_db",
+//   password: "password",
+//   port: 5432,
+// });
+
+// module.exports = pool;/
+
+require("dotenv").config();
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "india_geo_db",
-  password: "Meghana@123",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
